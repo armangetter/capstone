cc13753c3ce792c0955c7ee8fd53f581380ba826
armangetter <68382963+armangetter@users.noreply.github.com>
2020-08-07 20:35:03 +0700

    Final
    Hasil Akhir

--- app.py
+++ app.py
@@ -17,39 +17,46 @@ def scrap(url):
     soup = BeautifulSoup(url_get.content,"html.parser")
     
     #Find the key to get the information
-    table = soup.find(___) 
-    tr = table.find_all(___) 
+    table = soup.find('table',attrs={'class':'centerText newsTable2'}) 
+    tr = table.find_all('tr') 
 
     temp = [] #initiating a tuple
 
     for i in range(1, len(tr)):
         row = table.find_all('tr')[i]
-        #use the key to take information here
-        #name_of_object = row.find_all(...)[0].text
-
-
-
-
-
-
-        temp.append((___)) #append the needed information 
     
-    temp = temp[::-1] #remove the header
+        #get bulan
+        periode = row.find_all('td')[0].text
+        periode = periode.strip().replace('\xa0','-').replace('Agustus','08').replace('Juli','07') #for removing the excess whitespace
+        
+        #get jual
+        jual = row.find_all('td')[1].text
+        jual = jual.strip().replace(',','.') #for removing the excess whitespace
+    
+         #get beli
+        beli = row.find_all('td')[2].text
+        beli = beli.strip().replace(',','.')
+        temp.append((periode,jual,beli))  
+    
+        temp = temp[::-1] #remove the header
 
-    df = pd.DataFrame(temp, columns = (___)) #creating the dataframe
+    df = pd.DataFrame(temp, columns = ('periode','jual','beli')) #creating the dataframe
    #data wranggling -  try to change the data type to right data type
 
+    df['periode']=pd.to_datetime(df['periode'], dayfirst=True)
+    df['jual']=df['jual'].astype('float64')
+    df['beli']=df['beli'].astype('float64')
    #end of data wranggling
 
     return df
 
 @app.route("/")
 def index():
-    df = scrap(___) #insert url here
+    df = scrap('https://news.mifx.com/kurs-valuta-asing?kurs=JPY') #insert url here
 
     #This part for rendering matplotlib
     fig = plt.figure(figsize=(5,2),dpi=300)
-    df.plot()
+    df.pivot_table(values=['jual','beli'],index='periode',aggfunc='max').plot(kind='line')
     
     #Do not change this part
     plt.savefig('plot1',bbox_inches="tight") 
